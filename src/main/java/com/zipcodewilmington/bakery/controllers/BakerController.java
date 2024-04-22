d495d44f99b2aaaf8c3d24bc5b1c2f4ba6e1ddc4
Matt Kramer <matthew123003@gmail.com>
2024-04-22 09:34:21 -0400

    Started

--- src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
@@ -2,12 +2,15 @@ package com.zipcodewilmington.bakery.controllers;
 
 import com.zipcodewilmington.bakery.models.Baker;
 import com.zipcodewilmington.bakery.services.BakerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class BakerController {
     private BakerService service;
-
+    @Autowired
     public BakerController(BakerService service) {
         this.service = service;
     }
